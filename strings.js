ca8a24142ac4f26b33f10dfd3a16b3b117537e35
ronalynbaniagas <baniagasronalyn@gmail.com>
2024-09-15 06:15:33 +0800

    Delete strings.js

--- strings.js
+++
@@ -1,9 +0,0 @@
-let favActorFirstName = "Dylan";
-let favActorLastName = "Wang";
-let fullName = favActorFirstName + " " + favActorLastName;
-console.log(fullName);
-
-let uppercase = "dylan";
-let lowercase = "CAT";
-let message = "My favorite actor is DYLAN_WANG; his best show is Onlyn for love";
-console.log(message);
