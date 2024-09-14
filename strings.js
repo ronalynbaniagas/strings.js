2d52c23d5ed6a4c0dafac8f1371c2ee4943c6762
ronalynbaniagas <baniagasronalyn@gmail.com>
2024-09-15 06:16:06 +0800

    Add files via upload

---
+++ strings.js
@@ -0,0 +1,13 @@
+let favActorFirstName = "Dylan";
+let favActorLastName = "Wang";
+let fullName = favActorFirstName + " " + favActorLastName;
+console.log(fullName);
+
+let uppercase = "my favorite actor name is dylan";
+console.log(uppercase);
+
+let lowercase = "my favorite pet is CAT";
+console.log(lowercase);
+
+let message = "My favorite actor is DYLAN_WANG; his best show is Onlyn for love";
+console.log(message);
